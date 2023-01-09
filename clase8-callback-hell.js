f7d399350fe390b4931b540190d824974503e474
lmorozco <lorozco@valizant.com>
2023-01-09 13:01:03 -0800

    Callback Hell

---
+++ clase8-callback-hell.js
@@ -0,0 +1,52 @@
+function hola(nombre, miCallback) {
+    // console.log("Hola, soy una función asíncrona");
+    setTimeout(function () {
+        console.log("Hola, " + nombre);
+        miCallback(nombre);
+    }, 1000);
+}
+
+function hablar(callbackHablar) {
+    setTimeout(function () {
+        console.log("bla bla...");
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function () {
+        console.log("Adios", nombre);
+        otroCallback();
+    }, 1000);
+}
+
+function conversacion(nombre, veces, callback) {
+    if (veces > 0) {
+        hablar(function () {
+            conversacion(nombre, --veces, callback);
+        })
+    } else {
+        adios(nombre, callback);
+    }
+}
+console.log("Iniciando proceso...");
+hola("Luis", function(nombre) {
+    conversacion(nombre, 3, function(){
+        console.log("Proceso terminado");
+    });
+});
+
+// hola("Luis", function (nombre) {
+//     hablar(function () {
+//         hablar(function () {
+//             hablar(function () {
+//                 adios(nombre, function () {
+//                     console.log("Terminando proceso...");
+//                 });
+//             });
+//         });
+//     });
+// });
+
+// hola("Luis", function(){});
+// adios("Luis", function(){});
