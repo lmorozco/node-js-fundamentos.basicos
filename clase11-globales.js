6cc2e2f48a888e4fa52a3ca1e017d3154f81161e
lmorozco <lorozco@valizant.com>
2023-01-11 10:47:29 -0800

    Globals

---
+++ clase11-globales.js
@@ -0,0 +1,21 @@
+// let i=0;
+// let intervalo = setInterval(function (){
+//     console.log("Hola");
+//     if (i===3){
+//         clearInterval(intervalo);
+//     }
+//     i++;
+// }, 1000);
+
+// setImmediate(function (){
+//     console.log("Hola");
+// })
+
+// require(); //Requerir cualquier modulo
+
+//console.log(process);
+// console.log(__dirname);
+// console.log(__filename);
+
+global.miVariable = "Valorx";
+console.log(miVariable);
